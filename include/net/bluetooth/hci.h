1c88d07143742de7a08092cf381299bf76135288
neg2led <4232981+neg2led@users.noreply.github.com>
2022-08-18 00:12:32 +1000

    Applying Manjaro patch: 2001-Bluetooth-Add-new-quirk-for-broken-local-ext-features.patch

--- include/net/bluetooth/hci.h
+++ include/net/bluetooth/hci.h
@@ -228,6 +228,13 @@ enum {
 	 */
 	HCI_QUIRK_VALID_LE_STATES,
 
+	/* When this quirk is set, max_page for local extended features
+	 * is set to 1, even if controller reports higher number. Some
+	 * controllers (e.g. RTL8723CS) report more pages, but they
+	 * don't actually support features declared there.
+	 */
+	HCI_QUIRK_BROKEN_LOCAL_EXT_FTR_MAX_PAGE,
+
 	/* When this quirk is set, then erroneous data reporting
 	 * is ignored. This is mainly due to the fact that the HCI
 	 * Read Default Erroneous Data Reporting command is advertised,
